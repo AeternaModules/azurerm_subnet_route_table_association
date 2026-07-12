c2e60a5927fa6ad09962882bccc6e3898ed8b318
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "subnet_route_table_associations_id" {
+  description = "Map of id values across all subnet_route_table_associations, keyed the same as var.subnet_route_table_associations"
+  value       = { for k, v in azurerm_subnet_route_table_association.subnet_route_table_associations : k => v.id }
+}
 output "subnet_route_table_associations_route_table_id" {
   description = "Map of route_table_id values across all subnet_route_table_associations, keyed the same as var.subnet_route_table_associations"
   value       = { for k, v in azurerm_subnet_route_table_association.subnet_route_table_associations : k => v.route_table_id }
