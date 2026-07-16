b6b44f1ee4550faf42a6a463ce3aff957d1bd80c
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "subnet_route_table_associations_id" {
   description = "Map of id values across all subnet_route_table_associations, keyed the same as var.subnet_route_table_associations"
-  value       = { for k, v in azurerm_subnet_route_table_association.subnet_route_table_associations : k => v.id }
+  value       = { for k, v in azurerm_subnet_route_table_association.subnet_route_table_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "subnet_route_table_associations_route_table_id" {
   description = "Map of route_table_id values across all subnet_route_table_associations, keyed the same as var.subnet_route_table_associations"
-  value       = { for k, v in azurerm_subnet_route_table_association.subnet_route_table_associations : k => v.route_table_id }
+  value       = { for k, v in azurerm_subnet_route_table_association.subnet_route_table_associations : k => v.route_table_id if v.route_table_id != null && length(v.route_table_id) > 0 }
 }
 output "subnet_route_table_associations_subnet_id" {
   description = "Map of subnet_id values across all subnet_route_table_associations, keyed the same as var.subnet_route_table_associations"
-  value       = { for k, v in azurerm_subnet_route_table_association.subnet_route_table_associations : k => v.subnet_id }
+  value       = { for k, v in azurerm_subnet_route_table_association.subnet_route_table_associations : k => v.subnet_id if v.subnet_id != null && length(v.subnet_id) > 0 }
 }
 
